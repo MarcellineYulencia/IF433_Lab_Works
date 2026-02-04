6b6c7e9d3f7c2e52ae14ada8ebafeb7c6b7ca91a
cellinelucu <marcellineyolencia@gmail.com>
2026-02-04 20:19:09 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000107780_MarcellineYulencia/week01/GameStore.kt
+++ src/oop_00000107780_MarcellineYulencia/week01/GameStore.kt
@@ -3,4 +3,9 @@ package oop_00000107780_MarcellineYulencia.week01
 fun main() {
     val gameTitle = "Stardew Valley"
     val price = 70000
+
+    val finalPrice = calculateDiscount(price)
+    println("Price: $price, finalPrice: $finalPrice")
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) price * 80 / 100 else price * 90 / 100
