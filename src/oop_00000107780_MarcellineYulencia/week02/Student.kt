21a4b217a6ec225d1326a85aa643428aaefc7dfc
cellinelucu <marcellineyolencia@gmail.com>
2026-02-11 11:21:58 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000107780_MarcellineYulencia/week02/Student.kt
+++ src/oop_00000107780_MarcellineYulencia/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_00000107780_MarcellineYulencia.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (val name: String, val nim: String, var major: String, var gpa: Double = 0.0) {
     init {
         // Validasi Sederhana: Cek panjang NIM
         if (nim.length != 5) {
