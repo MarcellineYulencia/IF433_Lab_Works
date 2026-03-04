b1e4b617885e7b6c77c7378efb11a0ca38b93ecf
cellinelucu <marcellineyolencia@gmail.com>
2026-03-04 11:23:41 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_00000107780_MarcellineYulencia/week05/CreditCard.kt
@@ -0,0 +1,15 @@
+package oop_00000107780_MarcellineYulencia.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        println("Credit Card [$accountName] mencoba membayar Rp $amount")
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Pembayaran berhasil. Total pemakaian: Rp $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit kartu.")
+        }
+    }
+}
