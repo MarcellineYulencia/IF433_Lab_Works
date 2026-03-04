fa0efb727422a56df46c3f7c85ff1b0b40481ca7
cellinelucu <marcellineyolencia@gmail.com>
2026-03-04 11:17:46 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000107780_MarcellineYulencia/week05/EWallet.kt
@@ -0,0 +1,18 @@
+package oop_00000107780_MarcellineYulencia.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName){
+    override fun processPayment(amount: Double) {
+        println("E-Wallet [$accountName] mencoba membayar Rp $amount")
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil. Sisa saldo: Rp $balance")
+        } else {
+            println("Saldo tidak cukup.")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo sekarang: Rp $balance")
+    }
+}
