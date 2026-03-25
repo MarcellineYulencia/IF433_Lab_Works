86bbc0cca2e1f9aa307fc0cbec9975de067089c2
cellinelucu <marcellineyolencia@gmail.com>
2026-03-25 11:05:05 +0700

    week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_00000107780_MarcellineYulencia/week07/WeaponForge.kt
@@ -0,0 +1,13 @@
+package oop_00000107780_MarcellineYulencia.week07
+
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            return Weapon(GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON), 50)
+        }
+
+        fun forgeEpicSword(): Weapon {
+            return Weapon(GameItem("Pedang Naga Api", 100, ItemRarity.EPIC), 200)
+        }
+    }
+}
