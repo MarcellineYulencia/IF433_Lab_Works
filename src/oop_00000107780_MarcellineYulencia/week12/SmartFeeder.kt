a0571e74cf4ebf4d3501e2620e0c17427fbe502a
cellinelucu <marcellineyolencia@gmail.com>
2026-05-13 11:24:58 +0700

    week12: (task) handle feeding failure using onFailure

--- src/oop_00000107780_MarcellineYulencia/week12/SmartFeeder.kt
+++ src/oop_00000107780_MarcellineYulencia/week12/SmartFeeder.kt
@@ -54,5 +54,12 @@ fun main() {
         println(
             "Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr"
         )
+    }.onFailure { error ->
+        println(
+            "Peringatan ke Pemilik: ${error.message}"
+        )
+        println(
+            "(Opsional: Berikan chicken jerky secara manual)"
+        )
     }
 }
