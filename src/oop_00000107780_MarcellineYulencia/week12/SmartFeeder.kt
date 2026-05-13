27cca315476cacc45da22617d37282fa50f87f83
cellinelucu <marcellineyolencia@gmail.com>
2026-05-13 11:03:05 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000107780_MarcellineYulencia/week12/SmartFeeder.kt
+++ src/oop_00000107780_MarcellineYulencia/week12/SmartFeeder.kt
@@ -5,9 +5,12 @@ fun dispenseKibble(
     availableGram: Int,
     isJammed: Boolean
 ): Int {
-
     require(requestedGram > 0) {
         "Porsi kibble harus lebih dari 0 gr"
+    }
+
+    if (isJammed) {
+        throw DispenserJamException()
     }
 
     return availableGram
