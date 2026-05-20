d2f5959a6d04176d7f0b74bcc5736752c6c9baf1
cellinelucu <marcellineyolencia@gmail.com>
2026-05-20 11:45:55 +0700

    week13: (task) implement loadTrades with FileNotFoundException handling

--- src/oop_00000107780_MarcellineYulencia/week13/CryptoTradeLogger.kt
+++ src/oop_00000107780_MarcellineYulencia/week13/CryptoTradeLogger.kt
@@ -1,5 +1,6 @@
 package oop_00000107780_MarcellineYulencia.week13
 import java.io.File
+import java.io.FileNotFoundException
 
 data class TradeRecord(
     val id: Int,
@@ -36,5 +37,17 @@ fun saveTrades(trades: List<TradeRecord>, path: String) {
         trades.forEach {
             writer.println(it.toCsv())
         }
+    }
+}
+
+fun loadTrades(path: String): List<TradeRecord> {
+    return try {
+        File(path)
+            .readLines()
+            .mapNotNull { fromCsvTrade(it) }
+
+    } catch (e: FileNotFoundException) {
+        println("Error: File tidak ditemukan!")
+        emptyList()
     }
 }
